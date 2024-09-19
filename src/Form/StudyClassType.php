004184a0c221618b553e7ebdf07cee814e96c811
issamlammri <issam.lammri@live-session.fr>
2024-09-19 18:14:07 +0200

    update style for all Study Class pages

--- src/Form/StudyClassType.php
+++ src/Form/StudyClassType.php
@@ -6,16 +6,25 @@ use App\Entity\StudyClass;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class StudyClassType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('level')
-            ->add('speciality')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Class Name',
+                'attr' => ['placeholder' => 'Enter the name of the class']
+            ])
+            ->add('level', TextType::class, [
+                'label' => 'Class Level',
+                'attr' => ['placeholder' => 'Enter the level of the class']
+            ])
+            ->add('speciality', TextType::class, [
+                'label' => 'Speciality',
+                'attr' => ['placeholder' => 'Enter the speciality']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
