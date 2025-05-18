a9a25cff76ce4ff81479b346e8fcdd39df7615ad
issamlammri <issam.lammri@live-session.fr>
2025-05-18 14:37:55 +0200

    update role manager

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -20,7 +20,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_ADMIN')]
+#[IsGranted('ROLE_MANAGER')]
 #[Route('/payment')]
 class PaymentController extends AbstractController
 {
